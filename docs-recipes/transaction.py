c7f517531832bd406be913b6700882b355db91ad
Kimmeng <kimmeng.work@gmail.com>
2025-05-27 11:11:05 +0700

    try fi

--- docs-recipes/transaction.py
+++ docs-recipes/transaction.py
@@ -1,7 +1,29 @@
-from iroha2 import IrohaClient, KeyPair
+import iroha2
 
-client = IrohaClient("http://212.56.43.254:8080")
-keypair = KeyPair.from_json("""
+key_pair = iroha2.KeyPair.from_json("""
+{
+  "public_key": "ed0120CE7FA46C9DCE7EA4B125E2E36BDB63EA33073E7590AC92816AE1E861B7048B03",
+  "private_key": "802620CCF31D85E3B32A4BEA59987CE0C78E3B8E2DB93881468AB2435FE45D5C9DCD53"
+}
+""")
+
+account_id = "ed0120CE7FA46C9DCE7EA4B125E2E36BDB63EA33073E7590AC92816AE1E861B7048B03@wonderland"
+web_login = "mad_hatter"
+password = "ilovetea"
+api_url = "http://212.56.43.254:8080/"
+telemetry_url = "http://212.56.43.254:8180/"
+chain_id = "00000000-0000-0000-0000-000000000000"
+
+client = iroha2.Client.create(
+            key_pair,
+            account_id,
+            web_login,
+            password,
+            api_url,
+            chain_id)
+
+
+keypair = iroha2.KeyPair.from_json("""
 {
   "public_key": "ed0120CE7FA46C9DCE7EA4B125E2E36BDB63EA33073E7590AC92816AE1E861B7048B03",
   "private_key": "802620CCF31D85E3B32A4BEA59987CE0C78E3B8E2DB93881468AB2435FE45D5C9DCD53"
